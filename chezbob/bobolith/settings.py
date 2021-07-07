b7fdce37c5fe8387a10648bba84ae7a40fc20bbc
Dylan Lukes <dlukes@eng.ucsd.edu>
2021-07-07 16:57:43 -0700

    database support

--- chezbob/bobolith/settings.py
+++ chezbob/bobolith/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'chezbob.bobolith.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bobolith',
+        'USER': 'bobolith',
+        'PASSWORD': 'hunter2',
+        'HOST': 'localhost'
     }
 }
 
