f08eb6dbb0ff79fd71bb7fd1e12ed7d7e2ad7ce2
Dylan Lukes <dlukes@eng.ucsd.edu>
2021-07-20 13:40:23 -0700

    add appliance model, custom admin base

---
+++ chezbob/bobolith/apps/appliances/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class AppliancesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chezbob.bobolith.apps.appliances'
+    verbose_name = 'Appliances'
+    label = 'appliances'
