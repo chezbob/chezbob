7ed0708079f6a5d2c35c70e53cb0242a5e503fd1
Dylan Lukes <dlukes@eng.ucsd.edu>
2021-08-10 15:42:45 -0700

    accounts

--- chezbob/bobolith/apps/appliances/admin.py
+++ chezbob/bobolith/apps/appliances/admin.py
@@ -1,27 +1,38 @@
 from django.contrib import admin
 from django.contrib.admin import AdminSite
 
-from chezbob.bobolith.apps.appliances.models import Appliance
+from chezbob.bobolith.apps.appliances.models import Appliance, ApplianceLink
 
 
 class ApplianceAdmin(admin.ModelAdmin):
     fields = (
         'uuid',
         'name',
-        'consumer',
+        'consumer_path',
         'status',
         'last_connected_at',
-        'last_heartbeat_at'
+        'last_heartbeat_at',
+        'config'
     )
     readonly_fields = ('uuid',)
     list_display = (
         'name',
         'uuid',
-        'consumer',
+        'consumer_path',
         'status_icon',
         'last_connected_at',
         'last_heartbeat_at')
 
 
+class ApplianceLinkAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'key',
+        'src',
+        'dst'
+    )
+
+
 def register_default(admin_site: AdminSite):
     admin_site.register(Appliance, ApplianceAdmin)
+    admin_site.register(ApplianceLink, ApplianceLinkAdmin)
