bffd6d0445a0f5000338a2355d8fcca0ca3c5faa
Dylan Lukes <dlukes@eng.ucsd.edu>
2021-10-01 13:31:02 -0700

    msg_type -> message_type

--- chezbob/bobolith/apps/appliances/protocol.py
+++ chezbob/bobolith/apps/appliances/protocol.py
@@ -6,18 +6,21 @@ from django.conf import settings
 
 
 class MessageHeader:
-    __slots__ = ['msg_type', 'version']
+    __slots__ = ['message_type', 'version']
 
-    msg_type: str
     version: int
+    message_type: str
 
-    def __init__(self, msg_type, version=None):
-        self.msg_type = msg_type
+    def __init__(self, message_type, version=None):
+        self.message_type = message_type
         if version is None:
             self.version = settings.BOBOLITH_PROTOCOL_VERSION
 
     def to_json(self):
-        return {'msg_type': self.msg_type, 'version': self.version}
+        return {
+            'version': self.version,
+            'message_type': self.message_type,
+        }
 
 
 class Message(Protocol):
@@ -32,7 +35,7 @@ MESSAGE_TYPES: bidict[str, Type[Message]] = bidict({
 })
 
 
-def message_mixin(msg_type: str) -> Type[Message]:
+def message_mixin(message_type: str) -> Type[Message]:
     class MessageMixin(Message):
         __slots__ = ['header']
 
@@ -42,14 +45,14 @@ def message_mixin(msg_type: str) -> Type[Message]:
             super().__init__(header, **kwargs)
 
             if header is None:
-                self.header = MessageHeader(msg_type=msg_type)
+                self.header = MessageHeader(message_type=message_type)
 
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
         def __init_subclass__(cls, **kwargs):
             if cls not in MESSAGE_TYPES.inverse:
-                MESSAGE_TYPES[msg_type] = cls
+                MESSAGE_TYPES[message_type] = cls
 
         def to_json(self):
             return {slot: getattr(self, slot)
@@ -85,13 +88,13 @@ def _decode(json_dict):
     content = dict(json_dict)
     header_content = content.pop('header')
     if type(header_content) is not dict or \
-            'msg_type' not in header_content or \
-            header_content['msg_type'] not in MESSAGE_TYPES:
+            'message_type' not in header_content or \
+            header_content['message_type'] not in MESSAGE_TYPES:
         # This is a non-message dict, just return it.
         return json_dict
     header = MessageHeader(**header_content)
 
-    klass = MESSAGE_TYPES[header.msg_type]
+    klass = MESSAGE_TYPES[header.message_type]
     msg = klass(header=header, **content)
 
     return msg
