a4e1c3e69c555780c74f6bda69c443a292f6a034
enjhnsn2 <e5johnso@eng.ucsd.edu>
2021-10-08 18:39:25 -0500

    add dummy appliances to test more complex messages

--- util/relay_test_pos.py
+++ util/relay_test_pos.py
@@ -10,51 +10,21 @@ The appliances are linked POS1 -> Scanner1, POS2 -> Scanner2.
 
 import asyncio
 import json
-import uuid
 
 import websockets
 from string import Template
 
-URI_TEMPLATE = 'ws://192.168.1.5:8000/ws/appliances/{uuid}/'
+URI_TEMPLATE = 'ws://0.0.0.0:8000/ws/appliances/{uuid}/'
+DUMMY_POS1_UUID = 'd11ee641-f1f2-4044-9f80-d4679ee7977f'
 
-#DUMMY_POS1_UUID = 'd11ee641-f1f2-4044-9f80-d4679ee7977f'
-#DUMMY_POS2_UUID = 'cc87231e-e56b-4267-a8ab-cb19ad4d79af'
-DUMMY_SCANNER1_UUID = 'b03b4a62-57a8-4cb2-bf5d-ff4a42e7ab0d'
-DUMMY_SCANNER2_UUID = '53ac89d1-0272-4a57-bb21-d20d121a5ee1'
-
-
-async def connect_and_ping():
-    uri = URI_TEMPLATE.format(uuid=DUMMY_SCANNER1_UUID)
-    print(f"Connecting to: {uri} ")
+async def echo():
+    uri = URI_TEMPLATE.format(uuid=DUMMY_POS1_UUID)
+    print(f"Connecting to: {uri}")
 
     async with websockets.connect(uri) as ws:
-        await ws.send(json.dumps({
-            "header": {
-                "version": 0,
-                "msg_type": "relay",
-                "msg_id": str(uuid.uuid4())[:8],
-            },
-            "body": {
-                "dst" : "pos",
-                "payload": {"msg": "Hello POS!"},
-            }
-        }))
-        #greeting = await ws.recv()
-        #print(greeting)
-        print("Sent message to my POS!")
-        await ws.send(json.dumps({
-            "header": {
-                "version": 0,
-                "msg_type": "relay",
-                "msg_id": str(uuid.uuid4())[:8],
-            },
-            "body": {
-                "dst": "pos",
-                "payload": {"msg": "Hello again!"}
-            }
-        }))
-        #greeting = await ws.recv()
-        #print(greeting)
+        while True:
+            msg = await ws.recv()
+            print(msg)
 
 
-asyncio.get_event_loop().run_until_complete(connect_and_ping())
+asyncio.get_event_loop().run_until_complete(echo())
