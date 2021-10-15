9259dd00a72d2717b57d07ebeee067a1f30a16c3
enjhnsn2 <e5johnso@eng.ucsd.edu>
2021-10-15 13:14:17 -0500

    completed and tested get_price get_name and get_quantity

---
+++ python/chezbob/bobolith/apps/inventory/protocol.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass, asdict, field
+from chezbob.bobolith.apps.appliances.protocol import Message
+
+'''
+NOTE: these messages are used in chezbob.bobolith.apps.appliances.consumers
+'''
+
+# Inventory Querying Messages
+# -----------------------------
+
+# Appliance sends GetPriceMessage to server
+# Server responds with PriceResponse
+@dataclass(frozen=True)
+class GetPriceMessage(Message, msg_type='get_price'):
+    sku: str
+
+@dataclass(frozen=True)
+class PriceResponse(Message, msg_type='price_response'):
+    # price is a dict because Money can't otherwise be serialized
+    price: dict
+
+# Appliance sends GetNameMessage to server
+# Server responds with NameResponse
+@dataclass(frozen=True)
+class GetNameMessage(Message, msg_type='get_name'):
+    sku: str
+
+@dataclass(frozen=True)
+class NameResponse(Message, msg_type='name_response'):
+    name: str
+
+# Appliance sends GetQuantityMessage to server
+# Server responds with QuantityResponse
+@dataclass(frozen=True)
+class GetQuantityMessage(Message, msg_type='get_quantity'):
+    sku: str
+
+@dataclass(frozen=True)
+class QuantityResponse(Message, msg_type='quantity_response'):
+    quantity: int
