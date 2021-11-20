08c7237111ce1177d4286ef958ef0e2929dc1c2d
enjhnsn2 <e5johnso@eng.ucsd.edu>
2021-11-20 13:19:06 -0600

    add advanced mock scanner that differentiates between barcode and nfc

---
+++ util/advanced_test_scanner.py
@@ -0,0 +1,83 @@
+"""
+This script checks various aspects of the appliance connection process.
+
+NOTE: The UUID's used here will be loaded from chezbob/bobolith/apps/appliances/fixtures/dummy_appliances.json.
+      The command to do this is `python manage.py loaddata dummy`, or provide the full path instead of dummy.
+
+The appliances are linked POS1 -> Scanner1, POS2 -> Scanner2.
+
+"""
+
+import asyncio
+import json
+import uuid
+
+import websockets
+from string import Template
+
+URI_TEMPLATE = 'ws://0.0.0.0:8000/ws/appliances/{uuid}/'
+
+DUMMY_SCANNER1_UUID = 'b03b4a62-57a8-4cb2-bf5d-ff4a42e7ab0d'
+
+
+async def connect_and_ping():
+    uri = URI_TEMPLATE.format(uuid=DUMMY_SCANNER1_UUID)
+    print(f"Connecting to: {uri} ")
+    # Send 3 messages to relay
+    async with websockets.connect(uri) as ws:
+        
+        
+        barcode_msg = {
+            "header": {
+                "version": 0,
+                "msg_type": "barcode_scan_event",
+                "msg_id": str(uuid.uuid4())[:8],
+            },
+            "body": {
+                "barcode": "1234"
+            }
+        }
+
+        nfc_msg = {
+            "header": {
+                "version": 0,
+                "msg_type": "nfc_scan_event",
+                "msg_id": str(uuid.uuid4())[:8],
+            },
+            "body": {
+                "nfc": "abcd"
+            }
+        }
+        
+        # 1. Send a barcode message
+        await ws.send(json.dumps({
+            "header": {
+                "version": 0,
+                "msg_type": "relay",
+                "msg_id": str(uuid.uuid4())[:8],
+            },
+            "body": {
+                "link_key": "pos",
+                "payload": barcode_msg,
+            }
+        }))
+        # result = await ws.recv()
+        # print(result)
+
+        # 2. Send an nfc message
+        await ws.send(json.dumps({
+            "header": {
+                "version": 0,
+                "msg_type": "relay",
+                "msg_id": str(uuid.uuid4())[:8],
+            },
+            "body": {
+                "link_key": "pos",
+                "payload": nfc_msg,
+            }
+        }))
+        # result = await ws.recv()
+        # print(result)
+
+
+asyncio.get_event_loop().run_until_complete(connect_and_ping())
