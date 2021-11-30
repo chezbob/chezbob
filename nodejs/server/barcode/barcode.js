4448f96c5105b9c5665e1796c8e1775f69b3be86
John Renner <john@jrenner.net>
2021-11-29 16:49:46 -0800

    Read from actual barcode

--- nodejs/server/barcode/barcode.js
+++ nodejs/server/barcode/barcode.js
@@ -2,24 +2,49 @@ import { v4 as uuidv4 } from "uuid";
 import { ReconnectingSocket } from "../../common/reconnecting-socket.js";
 import { stdin as input, stdout as output } from "process";
 import * as readline from "readline/promises";
-
+import {HID} from 'node-hid';
 const rl = readline.createInterface({ input, output });
 let socket = await ReconnectingSocket.connect("barcode");
 
+
+function connect() {
+    try {
+        var handle = new HID("/dev/hidraw1");
+    } catch (e) {
+        console.log(e);
+        setTimeout(connect, 1000);
+        return;
+    }
+
+    handle.on('error', connect);
+    handle.on('data', d => {
+        let len = d.readUInt8(1);
+        let code = d.slice(5, 5 + len);
+        send(code.toString());
+    })
+}
+
+
+function send(barcode) {
+  console.log(`\n${barcode}`);
+  socket.send(
+    JSON.stringify({
+      header: {
+        to: "/pos",
+        id: uuidv4(),
+        type: "scan_event",
+      },
+      body: {
+        barcode,
+      },
+    })
+  );
+}
+
+connect();
 while (true) {
   let input = await rl.question("*barcode* ");
   if (socket.readyState === socket.OPEN) {
-    socket.send(
-      JSON.stringify({
-        header: {
-          to: "/pos",
-          id: uuidv4(),
-          type: "scan_event",
-        },
-        body: {
-          barcode: input,
-        },
-      })
-    );
+    send(input);
   }
 }
--- nodejs/server/inventory/db/seeds/dev/dummy_users.js
+++ nodejs/server/inventory/db/seeds/dev/dummy_users.js
@@ -1,6 +1,7 @@
 export async function seed(knex) {
   // Deletes ALL existing entries
   await knex("users").del();
-  await knex("users").insert([{ id: 1, barcode: "1111" }]);
+  await knex("transactions").del();
+  await knex("users").insert([{ id: 1, barcode: "21822057499743" }]);
   await knex("transactions").insert([{ id: 1, user_id: 1, cents: 1337 }]);
 }
