484dc2a288c781e2388e019007ff1a17e84b6ff5
John Renner <john@jrenner.net>
2021-12-01 18:04:48 -0800

    Disable caching on debug server

--- nodejs/debug.sh
+++ nodejs/debug.sh
@@ -7,7 +7,7 @@ tmux send 'node server/barcode.js' ENTER;
 tmux split-window;
 tmux send 'node server/inventory/inventory.js' ENTER;
 tmux split-window;
-tmux send 'npx http-server ./site' ENTER;
+tmux send 'npx http-server -c-1 ./site' ENTER;
 tmux select-layout main-vertical;
 tmux set -g mouse on;
 tmux a;
