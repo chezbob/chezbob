b5959999d22e52f04bb6b80962689d80ba6beae9
John Renner <john@jrenner.net>
2022-02-26 16:17:22 -0800

    Refactor site to make it easier to hack on

--- nodejs/site/pos/pos.js
+++ nodejs/site/pos/pos.js
@@ -1,282 +1,62 @@
 import { ReconnectingSocket } from "/common/reconnecting-socket.js";
+import { DefaultMode } from "./mode.js";
 
-let socket = await ReconnectingSocket.connect("ws://localhost:8080/", "pos");
-
-// Chez bob's UI is reasonably dynamic so we manage the state using this state object
-//   routines can call setState({...}) to update the state and trigger a rerender
-//
-// We attach it to the window for easy debugging but it should only be accessed through get_state()
-// to prevent awkward closure capture nonsense.
-const DEFAULT_STATE = {
-  user_info: null, // The info for the currently signed_in user
-  reset_timeout: null, // the time at which the session gets reset
-  purchases: [], // all purchased items
-  hint: null,
-  price_check: null, // item_info for a price check
-  error: null, // error text to display
-};
-
-window.STATE = DEFAULT_STATE;
-
-// Weird things can happen if you bind directly to the STATE object, so use get_state instead
-function get_state() {
-  return window.STATE;
-}
-
-// The render function is the only routine that updates the DOM. This means our UI is a function
-// from state to UI. This makes extending the UI much easier. This pattern is similar to those found
-// in React.
-window.render = (state) => {
-  if (state.user_info) {
-    document.getElementById("logout").disabled = false;
-    document.body.style.setProperty("--bob-color", "var(--chez-green");
-    setBalance(state.user_info?.balance);
-    setHint(state.user_info?.hint ?? "- Scan an item to purchase");
-    if (state.purchases.length > 0) {
-      setTitle("Purchases");
-      setContent(state.purchases.map(price_row).join("") + totals());
-    } else {
-      setTitle(null);
-      setContent(null);
-    }
-    set_timer_text();
-  } else {
-    document.getElementById("logout").disabled = true;
-    document.body.style.setProperty("--bob-color", "var(--chez-blue");
-    setBalance(null);
-    setHint(`
-      - Scan your ID to sign in
-      <br />
-      - Scan an item to price-check
-    `);
-    if (state.price_check) {
-      setTitle("Price Check");
-      setContent(price_row(state.price_check));
-    } else {
-      setTitle(null);
-      setContent(null);
-    }
-  }
-
-  document.getElementById("error").innerHTML = state.error;
+// Chez-bob is organized into "modes" which govern how the app behaves and looks.
+// These modes are defined in modes.js and account for most of the impelementation
+window.set_mode = (mode) => {
+  window.mode = mode;
+  window.mode.render();
 };
 
 // We need to render immediately because browsers are weird and will cache our data attributes
-render(get_state());
+set_mode(new DefaultMode());
 
-// Use setState to trigger a rerender.
-// Note: rendering is asynchronous so that multiple calls to
-// setState don't cost too much
-let rendering = false;
-function setState(new_state) {
-  STATE = new_state;
-  if (!rendering) {
-    setTimeout(() => render(STATE), 0);
-  }
-  rendering = true;
-  render(STATE);
-}
+// Rather than using Modes to control the socket, we put it directly on the window
+// and set up handlers that invoke mode methods like `on_scan`. This keeps us from having
+// to thread the socket object through all mode transitions, and from having to add and remove
+// handlers to the socket on the fly.
+window.socket = await ReconnectingSocket.connect("ws://localhost:8080/", "pos");
 
-// Determines how long the user has after any interaction before the session gets reset
-const SESSION_TIME = 30000;
-
+// The downside to the global socket system is that we have to establish these passthrough
+// handlers which invoke methods on the mode in response to a socket event. It's a small price to pay
+// for the simplicity we get by having modes be cheap and dumb.
 socket.on("scan_event", async (msg) => {
   try {
-    // prolong the session
-    add_session_time();
-
-    // Remove any old error messages for the sake of clarity
-    clear_error();
-
-    let info = await socket.request({
-      header: {
-        to: "inventory",
-        type: "info_req",
-      },
-      body: {
-        barcode: msg.body.barcode,
-      },
-    });
-
-    let state = get_state();
-
-    switch (info.header.type) {
-      case "item_info":
-        if (state.user_info) {
-          await purchase(info.body);
-        } else {
-          setState({
-            ...state,
-            price_check: info.body,
-          });
-        }
-        break;
-      case "user_info":
-        if (state.user_info) {
-          set_error("Already signed in");
-        } else {
-          login(info.body);
-        }
-        break;
-    }
+    window.mode.on_scan && (await window.mode.on_scan(msg));
   } catch (e) {
     // If it has an `error` member, then it's an error from the socket request
     // and should be displayed
     if (e.error) {
-      set_error(e.error);
+      window.mode.set_error(e.error);
     } else {
       console.error(e);
     }
   }
 });
 
-/** User management */
-
-function login(user_info) {
-  console.log("LOGIN", user_info);
-  setState({
-    ...get_state(),
-    user_info,
-    purchases: [],
-    price_check: null,
-  });
-}
-
-function reset() {
-  setState(DEFAULT_STATE);
-}
-
-document.getElementById("logout").addEventListener("click", reset);
-
-function curr_user() {
-  return get_state().user_info;
-}
-
-async function purchase(item_info) {
-  let resp = await socket.request({
-    header: {
-      to: "inventory",
-      type: "purchase",
-    },
-    body: {
-      user_id: curr_user().id,
-      item_id: item_info.id,
-    },
-  });
-
-  setState({
-    ...get_state(),
-    purchases: [...get_state().purchases, resp.body.item],
-    user_info: {
-      ...get_state().user_info,
-      balance: resp.body.balance,
-    },
-  });
-}
-
-function add_session_time() {
-  setState({
-    ...get_state(),
-    reset_timeout: Date.now() + SESSION_TIME,
-  });
-}
-
-function totals() {
-  const sum = get_state().purchases.reduce((sum, i) => sum + i.cents, 0);
-  return `<br><div class='totals'>
-      <div>Total: </div>
-      <div>${dollars(sum)}</div>
-    </div>`;
-}
-
-function price_row(item) {
-  return `<div class='price-row'>
-        <span class='price-name'>
-            ${item.name}
-        </span>
-        <span class="dots"></span>
-        <span class='price-cost'>
-            ${dollars(item.cents)}
-        </span>
-    </div>`;
-}
-
-function dollars(cents) {
-  let d = Math.floor(cents / 100);
-  let c = Math.abs(cents) % 100;
-  return `${d}.${c < 10 ? "0" + c : c}`;
-}
-
-function set_timer_text() {
-  if (curr_user() !== null) {
-    const millis_remaining = get_state().reset_timeout - Date.now();
-    console.log("MILLIS REMAINING", millis_remaining);
-    document.getElementById("logout").innerText = `Sign Out(${Math.floor(
-      millis_remaining / 1000
-    )})`;
-  }
-}
-
-// It's just easier to leave the reset timer always running and have it do nothing
-// if the timeout isn't set
+//
+// IMPORTANT!
+//
+// Some modes have timing-specific properties, this could be a session that needs to timeout
+// or an error that needs to disappear at some point. Rather than making modes set up and teardown
+// interval handlers, we just do that globally.
+//
+// This is a little funky because some modes need to render every second (e.g. to show session-time remainig).
+// To solve this problem inelegantly, but succinctly, we just rerender every second.
+// Typically, updates to the mode should not wait for this cycle, but should explicitly render immediately.
+// If you notice that the app is seemingly slow to update, it's likely that you forgot to explicitly render
+// and were saved by this per-second-reload
 setInterval(() => {
-  if (get_state().reset_timeout !== null) {
-    const millis_remaining = get_state().reset_timeout - Date.now();
-    if (millis_remaining <= 0) {
-      reset();
-    }
+  // If the session times out, reset it
+  if (window.mode.timeout && Date.now() > window.mode.timeout) {
+    set_mode(new DefaultMode());
   }
-  render(get_state());
+
+  // If the error times out, clear it
+  if (window.mode.error_timeout && Date.now() > window.mode.error_timeout) {
+    window.mode.error = window.mode.error_timeout = undefined;
+  }
+
+  // Rerender
+  window.mode.render();
 }, 1000);
-
-function setTitle(title) {
-  if (title === null) {
-    delete document.getElementById("content").dataset.title;
-  } else {
-    document.getElementById("content").dataset.title = title;
-  }
-}
-
-function setContent(content) {
-  document.getElementById("content").innerHTML = content;
-}
-
-function setHint(content) {
-  document.getElementById("hint").innerHTML = content;
-}
-
-function setBalance(cents) {
-  const elem = document.getElementById("balance");
-  if (cents) {
-    elem.dataset.balance = dollars(cents);
-    if (cents > 0) {
-      elem.classList.add("positive");
-      elem.classList.remove("negative");
-    } else {
-      elem.classList.add("negative");
-      elem.classList.remove("positive");
-    }
-  } else {
-    delete document.getElementById("balance").dataset.balance;
-  }
-}
-
-
-let error_timeout = null;
-function set_error(txt) {
-  setState({
-    ...get_state(),
-    error: txt,
-  });
-  if (error_timeout) {
-    clearTimeout(error_timeout);
-  }
-  error_timeout = setTimeout(clear_error, 5000);
-}
-
-function clear_error() {
-  setState({
-    ...get_state(),
-    error: null,
-  });
-}
