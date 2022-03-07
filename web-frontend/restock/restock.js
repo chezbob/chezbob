78e95d88027fbe5d765fd3f355911afe1cbaf4a9
John Renner <john@jrenner.net>
2022-03-07 15:21:50 -0800

    Update restock to use bulk numbers

--- web-frontend/restock/restock.js
+++ web-frontend/restock/restock.js
@@ -11,17 +11,37 @@ let socket = await (async () => {
   return await ReconnectingSocket.connect(`ws://${host}:${port}/`, "pos");
 })();
 
-socket.on("scan_event", async (msg) => {
+let buffer = "";
+
+// Since we're just listening for any keypress, we'll want to clear spurious keypresses.
+// We'll say that all barcode scanners should emit their letters less than 1 second apart
+// This is super generous. If a fully barcode has not been read after 1 second, start over.
+let timeout;
+document.onkeydown = ({key}) => {
+  // Only accept input if no input element is selected
+  if (window.activeElement === undefined && key.match(/^[0-9]$/)){
+    clearTimeout(timeout);
+    buffer += key
+    if (buffer.length === 12) {
+      scan(buffer);
+      buffer = "";
+    } else {
+      timeout = setTimeout(() => buffer = '', 1000);
+    }
+  }
+};
+
+async function scan(upc) {
   report("");
 
   try {
     let info = await socket.request({
       header: {
-        to: "/inventory",
+        to: "inventory",
         type: "info_req",
       },
       body: {
-        barcode: msg.body.barcode,
+        barcode: upc,
       },
     });
     switch (info.header.type) {
@@ -34,29 +54,37 @@ socket.on("scan_event", async (msg) => {
         break;
     }
   } catch (e) {
-    render({ barcode: msg.body.barcode });
+    render({ barcode: upc});
   }
-});
+};
 
 function render(obj) {
   let html = `
-            <fieldset name="fields" id="fields">
+            <fieldset name="fields" id="fields" oninput="display_cost()">
             <input type="hidden" name="id" value="${obj?.id ?? ""}"/>
             <br />
             <label for="barcode">Barcode: </label> 
             <input disabled type="text" name="barcode" value="${
               obj?.barcode ?? ""
-            }"/>
+            }"/> ${obj.cents !== undefined ? `Current Cost: ${dollars(obj.cents)}`: ''}
             <br />
             <label for="name">Name: </label> 
             <input required minlength=1 title="Cannot be empty" type="text" name="name" value="${
               obj?.name ?? ""
             }"/>
             <br />
-            <label for="cents">Cost: </label> 
-            <input required type="number" name="cents" value="${
-              obj?.cents ?? ""
-            }"/>
+            <label for="bulk_cost">Bulk Count: </label> 
+            <input required type="number" name="bulk_count" value=""/>
+            <br />
+            <label for="bulk_cost">Bulk Cost: </label> 
+            <input required type="text" pattern="\\d+\.\\d\\d" name="bulk_cost" value=""/>
+            <br />
+            <label for="bulk_cost">Tax?: </label> 
+            <input type="checkbox" name="tax" />
+            <br />
+
+            <div>Calculated Cost: <span id="cost"></span></div>
+
             <button id="submit" type="submit">${
               obj?.id === undefined ? "Create" : "Update"
             }</button>
@@ -67,31 +95,61 @@ function render(obj) {
   document.getElementById("fields").disabled = false;
 }
 
+function form_values() {
+  let values = {};
+  document.querySelectorAll("#fields > input").forEach((el) => {
+    if (el.type === "checkbox") {
+      values[el.name] = el.checked;
+    } else {
+      values[el.name] = el.value;
+    }
+  });
+  return values;
+
+}
+
+function calculate_cost_in_cents() {
+  const form = form_values();
+  const bulk_cost = Number.parseFloat(form.bulk_cost);
+  const tax_cost = form.tax ? bulk_cost * 1.077 : bulk_cost;
+  const overhead_cost = tax_cost * 1.2;
+  const unit_price = overhead_cost / Number.parseInt(form.bulk_count);
+  return Math.ceil(unit_price * 100);
+}
+
+window.display_cost = () => {
+  document.getElementById('cost').innerHTML = "$" + dollars(calculate_cost_in_cents());
+};
+
+function dollars(cents) {
+  let d = Math.floor(cents / 100);
+  let c = Math.abs(cents) % 100;
+  return `${d}.${c < 10 ? "0" + c : c}`;
+}
+
 async function submit(ev) {
   ev.preventDefault();
   document.getElementById("fields").disabled = true;
 
-  let form_values = {};
-  document.querySelectorAll("#fields > input").forEach((el) => {
-    form_values[el.name] = el.value;
-  });
+  let form = form_values();
 
   try {
     await socket.request({
       header: {
-        to: "/inventory",
+        to: "inventory",
         type: "update_info",
       },
       body: {
-        id: form_values["id"] === "" ? null : form_values["id"],
-        name: form_values["name"],
-        cents: form_values["cents"],
-        barcode: form_values["barcode"],
+        id: form["id"] === "" ? null : form["id"],
+        name: form["name"],
+        cents: calculate_cost_in_cents(),
+        barcode: form["barcode"],
       },
     });
     report("Success!");
   } catch (e) {
-    report(e.error);
+    console.log(e);
+    report(e);
   }
 }
 
