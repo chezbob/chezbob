da209700210b0d01138f7b660c2eeeeb32d2c069
John Renner <john@jrenner.net>
2022-03-10 14:16:05 -0800

    Fix double negative signs in balance

--- web-frontend/pos/money.js
+++ web-frontend/pos/money.js
@@ -11,7 +11,7 @@ export function price_row(item) {
 }
 
 export function dollars(cents) {
-  let d = Math.trunc(cents / 100);
+  let d = Math.abs(Math.trunc(cents / 100));
   let c = Math.abs(cents) % 100;
   return `${cents < 0 ? "-" : ""}${d}.${c < 10 ? "0" + c : c}`;
 }
