5f6b11b03229010ef980283148b70fe6a9071402
alex bellon <github@alex-bellon.com>
2022-04-01 15:18:54 -0500

    Add RELAY_SERVER variable to debug commands (#9)
    * Add RELAY_SERVER variable to debug commands
    
    * Fixed quote issue in debug script

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 export DEBUG=true;
-export RELAY_SERVER="ws://localhost:8080/"
-tmux new-session -d -s chezbob 'node server/relay.js';
+export RELAY_SERVER='ws://localhost:8080/'
+tmux new-session -d -s chezbob "node server/relay.js";
 tmux split-window;
-tmux send 'SERVICE_IDENT="barcode" DESTINATION_IDENT="pos" node devices/barcode.js' ENTER;
+tmux send "SERVICE_IDENT='barcode' DESTINATION_IDENT='pos' RELAY_SERVER=$RELAY_SERVER node devices/barcode.js" ENTER;
 tmux split-window;
-tmux send 'DEPLOYMENT_MODE="development" node server/inventory.js' ENTER;
+tmux send "DEPLOYMENT_MODE='development' RELAY_SERVER=$RELAY_SERVER node server/inventory.js" ENTER;
 tmux split-window;
-tmux send 'npx http-server -c-1 ./web-frontend' ENTER;
+tmux send "npx http-server -c-1 ./web-frontend" ENTER;
 tmux select-layout main-vertical;
 tmux set -g mouse on;
 tmux a;
