b90acf13aedcb77cf1c5ec193b38f4302036c4ab
alex bellon <github@alex-bellon.com>
2022-04-01 13:12:26 -0700

    Add RELAY_SERVER variable to debug commands

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -3,9 +3,9 @@ export DEBUG=true;
 export RELAY_SERVER="ws://localhost:8080/"
 tmux new-session -d -s chezbob 'node server/relay.js';
 tmux split-window;
-tmux send 'SERVICE_IDENT="barcode" DESTINATION_IDENT="pos" node devices/barcode.js' ENTER;
+tmux send 'SERVICE_IDENT="barcode" DESTINATION_IDENT="pos" RELAY_SERVER=$RELAY_SERVER node devices/barcode.js' ENTER;
 tmux split-window;
-tmux send 'DEPLOYMENT_MODE="development" node server/inventory.js' ENTER;
+tmux send 'DEPLOYMENT_MODE="development" RELAY_SERVER=$RELAY_SERVER node server/inventory.js' ENTER;
 tmux split-window;
 tmux send 'npx http-server -c-1 ./web-frontend' ENTER;
 tmux select-layout main-vertical;
