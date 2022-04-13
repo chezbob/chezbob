ecf5e0044d2d04b99d0c7fb7ef4460a25b028d93
djrenren <john@jrenner.net>
2022-04-13 13:16:03 -0700

    Implement interactive cash prompt for debugging (#23)

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -5,6 +5,8 @@ tmux new-session -d -s chezbob "DEPLOYMENT_MODE='development' HTTP_PORT=8080 nod
 tmux split-window;
 tmux send "SERVICE_IDENT='barcode' DESTINATION_IDENT='pos' RELAY_SERVER=$RELAY_SERVER node scripts/debug_barcode" ENTER;
 tmux split-window;
+tmux send "SERVICE_IDENT='cash' DESTINATION_IDENT='pos' RELAY_SERVER=$RELAY_SERVER node scripts/debug_cash" ENTER;
+tmux split-window;
 tmux send "DEPLOYMENT_MODE='development' RELAY_SERVER=$RELAY_SERVER node inventory" ENTER;
 tmux split-window;
 tmux send "DEPLOYMENT_MODE='development' HTTP_PORT=8081 node web" ENTER;
