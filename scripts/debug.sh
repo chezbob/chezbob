5821234bb68f89ebd389dbe6586bccb31f27cd6a
John Renner <john@jrenner.net>
2022-05-16 09:24:45 -0700

    Consolidate web folders

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export DEBUG=true;
 export RELAY_SERVER='ws://localhost:8080/'
-tmux new-session -d -s chezbob "DEPLOYMENT_MODE='development' HTTP_PORT=8080 node app-server";
+tmux new-session -d -s chezbob "DEPLOYMENT_MODE='development' HTTP_PORT=8080 node web/internal.js";
 tmux split-window;
 tmux send "SERVICE_IDENT='barcode' DESTINATION_IDENT='pos' RELAY_SERVER=$RELAY_SERVER node scripts/debug_barcode" ENTER;
 tmux split-window;
--- web/external.js
+++ web/external.js
@@ -1,11 +1,16 @@
 /*
-    Web is chezbob's public internet presence.
+    external is chezbob's public internet presence.
 */
 
 import express from "express";
 import { hybridServer } from "hybrid-http-server";
 
 const app = express();
+
+// Disallow access to the internal apps
+app.use("/internal", (req, res) => {
+    res.status(403).end('403 Forbidden');
+});
 
 // Static file serving
 const __dirname = new URL(".", import.meta.url).pathname;
