88113d53393981ee878c10cd09490b251d159fd9
Marcus Fedarko <mfedarko@ucsd.edu>
2023-02-20 19:08:20 -0800

    documentation

--- web/static/apps/ah/ah-index.js
+++ web/static/apps/ah/ah-index.js
@@ -1,9 +1,9 @@
-// Will be run from the index page, to only sometimes show this. Based on the
-// wos.js code (thanks Eric) because i forgor how node works
+// Will be run from the index page. Based on the wos.js code (thanks Eric)
+// because i forgor how node works
 function onlyShowIfLucky() {
   let r = Math.random(); // in [0, 1)
   if (r <= 0.2) {
-    let ahp = document.getElementById("alt-hist");
+    let ahp = document.getElementById("ah");
     ahp.innerHTML = 
         `<a href="apps/ah">Chez Bob's History (real)</a>`;
   }
