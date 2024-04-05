5324d4c29d305873c7f59d5422edb568b2225f96
Marcus Fedarko <mfedarko@ucsd.edu>
2024-04-05 11:45:50 -0700

    Link to "Welcome to Chez Bob" doc from homepage
    Addresses
    https://ucsdcse.slack.com/archives/C0447VCTYRK/p1709267870035989?thread_ts=1709255895.940139&cid=C0447VCTYRK

--- web/static/apps/ah/ah-index.js
+++ web/static/apps/ah/ah-index.js
@@ -2,7 +2,7 @@
 // because i forgor how node works
 function onlyShowIfLucky() {
   let r = Math.random(); // in [0, 1)
-  if (r <= 0.2) {
+  if (r <= 0.1) {
     let ahp = document.getElementById("ah");
     ahp.innerHTML = 
         `<a href="apps/ah">Chez Bob's History (real)</a>`;
