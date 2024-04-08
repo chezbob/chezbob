297e48960697bb272470609518ce7f5782886080
alex-bellon <alexrbellon@gmail.com>
2024-04-08 13:50:59 -0700

    update

--- web/static/apps/wos/wos.js
+++ web/static/apps/wos/wos.js
@@ -44,38 +44,10 @@ async function display_debt() {
   }
 }
 
-async function display_assets() {
-  let response = await fetch("/api/wos/totalassets");
-  let asset_element = document.getElementById("assets");
-  try {
-    let debt = await response.json();
-    console.log(debt)
-    asset_element.innerHTML = `Total Assets: $${debt.total_assets}`;
-  } catch (e) {
-    asset_element.innerHTML = `<div id="message">Total assets can't be displayed today!</div>`;
-  }
-}
-
-async function display_balance() {
-  let response = await fetch("/api/wos/totalbalance");
-  let balance_element = document.getElementById("balance");
-  try {
-    let debt = await response.json();
-    console.log(debt)
-    balance_element.innerHTML = `Overall Balance: $${debt.total_balance}`;
-  } catch (e) {
-    balance_element.innerHTML = `<div id="message">Total balance can't be displayed today!</div>`;
-  }
-}
-
 // display wall info when loading the page
 display_wall();
 display_debt();
-display_assets();
-display_balance();
 
 // refresh the wall
 setInterval(display_wall, REFRESH_INTERVAL); //ms
 setInterval(display_debt, REFRESH_INTERVAL); //ms
-setInterval(display_assets, REFRESH_INTERVAL); //ms
-setInterval(display_balance, REFRESH_INTERVAL); //ms
