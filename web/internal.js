b81371b1377c3fe76e252d60df3cbaf02c8d5165
Nagendra Kumar Jamadagni <nagendrajamadagni@gmail.com>
2025-11-06 21:36:28 -0800

    admin-panel: Inserted backend routes needed for admin panel

--- web/internal.js
+++ web/internal.js
@@ -1,4 +1,5 @@
 /*
+      <li><a href="apps/restock">restock tool</a></li>
     App Server is responsible for hosting web apps for internal use within chezbob.
     This includes such things as the POS interface. The full list can be found in `app-server/static/apps`.
 
@@ -10,17 +11,19 @@ import { get_user_by_email, create_user, search_inventory, reset_password_by_id 
 import express from "express";
 import RelayServer from "relay-server";
 import { hybridServer } from "hybrid-http-server";
+import apiRoutes from "./api.js"; // <- import the router
 
 const app = express();
 const relay = new RelayServer();
 const api = express.Router();
 
-// Redirect to the internal landing page.
-app.get("/", (_, res) => {
-  res.redirect(302, "/internal/");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Static file serving
+// Redirect to internal landing page
+app.get("/", (_, res) => res.redirect(302, "/internal/"));
+
+// Static files
 const __dirname = new URL(".", import.meta.url).pathname;
 app.use(express.static(__dirname + "/static"));
 
@@ -61,3 +64,4 @@ app.use("/api", api);
 
 let server = hybridServer(app);
 server.on("upgrade", (...args) => relay.handleUpgrade(...args));
+
